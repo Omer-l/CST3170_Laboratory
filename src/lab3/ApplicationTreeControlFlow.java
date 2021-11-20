6f512408409094d2225717583827171b40b85b13
Omer Kacar <omerkacar@Omers-MacBook-Pro.local>
2021-11-20 23:35:33 +0000

    feat(core): add clearer print method to ApplcationTreeControl.java lab3
    This is so that the code is cleaner and can easily understood.

--- src/lab3/ApplicationTreeControlFlow.java
+++ src/lab3/ApplicationTreeControlFlow.java
@@ -1,39 +1,68 @@
 package lab3;
 
+import java.util.Arrays;
+
 public class ApplicationTreeControlFlow {
 
 	public static void main(String[] args) {
 		FileReaderLab3 fileReader = new FileReaderLab3("lenseData.txt");
 		DecisionTableRenderer decisionTable = new DecisionTableRenderer(fileReader.getData()); //render in the data.
 
-		for(int i : decisionTable.getCategories())
-			if(i == 1)
-				System.out.println("hard");
-			else if(i == 2)
-				System.out.println("soft");
-			else if(i == 3)
-				System.out.println("none");
-					
-	}
+		int[] expectedCategories = decisionTable.getCategories();
 
-	public static int[] getCategory(int[][] arr) {
+		int[][] featuresInputs = decisionTable.getFeaturesInputs();
+		int[] actualCategories = new int[featuresInputs[0].length];
 
-		int[] category = new int[arr.length];
+		for(int rowIterator = 0; rowIterator < featuresInputs[0].length; rowIterator++) {
+			int f1 = featuresInputs[0][rowIterator];
+			int f2 = featuresInputs[1][rowIterator];
+			int f3 = featuresInputs[2][rowIterator];
+			int f4 = featuresInputs[3][rowIterator];
 
-		for (int i = 0; i < arr.length; i++) {
-			category[i] = getRowCategory(arr[i]);
+			actualCategories[rowIterator] = getRowCategory(f1, f2, f3, f4);
 		}
 
-		return category;
+		System.out.printf("%-20s%-20s\n", "EXPECTED", "ACTUAL");
 
+		for(int categoryIterator = 0; categoryIterator < expectedCategories.length; categoryIterator++) {
+			String expectedCategory = categoryToString(expectedCategories[categoryIterator]);
+			String actualCategory = categoryToString(actualCategories[categoryIterator]);
+			System.out.printf("%-20s%-20s\n", expectedCategory, actualCategory);
+		}
 	}
 
-	public static int getRowCategory(int[] features) {
-		int f4 = features[3];
-		int f3 = features[2];
-		int f2 = features[1];
-		int f1 = features[0];
-		
+	/**
+	 * This function turns the category class into its respective lense type.
+	 * @param category	category class number
+	 * @return			lense type
+	 */
+	public static String categoryToString(int category) {
+		StringBuilder sb = new StringBuilder();
+
+			if(category == 1)
+				sb.append("hard");
+			else if(category == 2)
+				sb.append("soft");
+			else if(category == 3)
+				sb.append("none");
+
+		return sb.toString();
+	}
+
+//	public static int[] getCategory(int[][] arr) {
+//
+//		int[] category = new int[arr.length];
+//
+//		for (int i = 0; i < arr.length; i++) {
+//			category[i] = getRowCategory(arr[i]);
+//		}
+//
+//		return category;
+//
+//	}
+
+	public static int getRowCategory(int f1, int f2, int f3, int f4) {
+
 		if (f4 == 1) {
 			return 3;
 		} else if (f4 == 2) {
