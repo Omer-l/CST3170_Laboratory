340832c6712b8d5d78d19bc8bee8833114c842bf
Omer Kacar <omerkacar@Omers-MacBook-Pro.local>
2021-11-21 09:41:18 +0000

    feat(core): add necessary functions ApplicationHardCodedTree.java lab3
    Add traverseDecisionTree() to get the lens category. Add prompt() to get the
    decisions from the user in order to traverse the tree. Add categoryToString()
    to output the correct information regarding the lens required for the user.

--- src/lab3/ApplicationHardCodedTree.java
+++ src/lab3/ApplicationHardCodedTree.java
@@ -12,15 +12,46 @@ public class ApplicationHardCodedTree {
 
         buildTree(decisionTree);
 
-        System.out.println(decisionTree.getChildren().get(1));
+        //gets the decisions by prompting the user with questions.
+        int[] decisions = prompt(decisionTree);
+        int f1 = decisions[0];
+        int f2 = decisions[1];
+        int f3 = decisions[2];
+        int f4 = decisions[3];
+
+        //traverse decision tree.
+        int category = traverseDecisionTree(decisionTree, f1, f2, f3, f4);
+
+        System.out.println(categoryToString(category));
     }
 
     /**
-     * builds tree, adds in a breadth-first search order.
+     * prompts the user to enter the feature inputs.
+     * @param decisionTree  decision tree to find the right lens class for the user.
+     * @return              user's decisions, these are the indexes of the children in each TreeNode
+     */
+    public static int[] prompt(TreeNode<String> decisionTree) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the feature 1's input: ");
+        int f1 = input.nextInt()-1;
+        System.out.print("\nEnter the feature 2's input: ");
+        int f2 = input.nextInt()-1;
+        System.out.print("\nEnter the feature 3's input: ");
+        int f3 = input.nextInt()-1;
+        System.out.print("\nEnter the feature 4's input: ");
+        int f4 = input.nextInt()-1;
+
+        int[] decisions = {f1, f2, f3, f4};
+
+        return decisions;
+    }
+
+    /**
+     * builds tree, adds decisions in a breadth-first search order. Level by level, for each branch that contains a feature 'F'.
      * @param root
      */
     public static void buildTree(TreeNode<String> root) {
-
         /* LEVEL 0*/
 
         //F3 -> Nodes to create for F4 AKA root
@@ -64,25 +95,34 @@ public class ApplicationHardCodedTree {
         //F2 -> Nodes to create for third branches' F2's children
         TreeNode<String> F2Level4Branch3Child1 = new TreeNode<>("1", F1Level3Child3);
         TreeNode<String> F2Level4Branch3Child2 = new TreeNode<>("3", F1Level3Child3);
-
     }
 
     /**
-     * prompts the user to enter the feature inputs.
-     * @param decisionTree  decision tree to find the right lens class for the user.
+     * traverses the tree based on the decisions made by the user.
+     * @param treeRoot  root of the tree
+     * @param f1    	feature 1's input
+     * @param f2    	feature 2's input
+     * @param f3    	feature 3's input
+     * @param f4    	feature 4's input
+     * @return          type of lens user should use.
      */
-    public static void prompt(TreeNode<String> decisionTree) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter the feature 1's input: ");
-        int f1 = input.nextInt()-1;
-        System.out.print("\nEnter the feature 2's input: ");
-        int f2 = input.nextInt()-1;
-        System.out.print("\nEnter the feature 3's input: ");
-        int f3 = input.nextInt()-1;
-        System.out.print("\nEnter the feature 4's input: ");
-        int f4 = input.nextInt()-1;
+    public static int traverseDecisionTree(TreeNode<String> treeRoot, int f1, int f2, int f3, int f4) {
+        int category = -1;
 
-        //traverse tree.
+        TreeNode<String> currentTreeNode = treeRoot;
+
+        while(currentTreeNode.getChildren().size() != 0) { //traverse decisions made until there are no children.
+            switch(currentTreeNode.getData()) {
+                case "F4": currentTreeNode = currentTreeNode.getChildren().get(f4); break;
+                case "F3": currentTreeNode = currentTreeNode.getChildren().get(f3); break;
+                case "F2": currentTreeNode = currentTreeNode.getChildren().get(f2); break;
+                case "F1": currentTreeNode = currentTreeNode.getChildren().get(f1); break;
+            }
+        }
+
+        category = Integer.parseInt(currentTreeNode.getData());
+
+        return category;
     }
 
     /**
@@ -94,12 +134,13 @@ public class ApplicationHardCodedTree {
         StringBuilder sb = new StringBuilder();
 
         if(category == 1)
-            sb.append("hard");
+            sb.append("The patient should be fitted with hard contact lenses");
         else if(category == 2)
-            sb.append("soft");
+            sb.append("The patient should not be fitted with contact lenses");
         else if(category == 3)
-            sb.append("none");
+            sb.append("The patient should not be fitted with contact lenses");
 
         return sb.toString();
     }
 }
+
